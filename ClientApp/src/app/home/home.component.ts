faa68269f18149a7b5acbd7f2dcac5a32521ba69
Aaron Psujek <ap058838@abcsupply.com>
2021-10-26 16:46:19 -0500

    client filters response messages

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
+import { ReturnObj } from '../models/returnObj.model';
 
 @Component({
   selector: 'app-home',
@@ -9,6 +10,7 @@ import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
 export class HomeComponent {
   private conn: HubConnection;
   private username: string;
+  connId: string = 'test';
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.username = Math.floor(Math.random() * 10000) + '@gmail.com';
@@ -22,8 +24,8 @@ export class HomeComponent {
       .build();
     console.log('connecting...')
 
-    this.conn.on("userAdded", this.onUserAdded)
-    this.conn.on("connected", this.onConnected)
+    this.conn.on("userAdded", this.onUserAdded.bind(this))
+    this.conn.on("connected", this.onConnected.bind(this))
 
     this.conn.start()
     .then((resp) => {
@@ -35,6 +37,7 @@ export class HomeComponent {
   }
 
   invokeEvent() {
+    console.log(this.connId)
     this.http.get(this.baseUrl + 'SignalRConnection/' + this.username).subscribe(result => {
       console.log(result)
     }, error => console.error(error));
@@ -46,11 +49,16 @@ export class HomeComponent {
     }, error => console.error(error));
   }
 
-  onUserAdded(userId) {
-    console.log('user added', userId)
+  onUserAdded(returnObj: ReturnObj) {
+    if (returnObj.TriggeringUser.toLowerCase() !== this.username.toLowerCase()) {
+      alert(`${returnObj.TriggeringUser} added to group`)
+    } else {
+      console.log('came from same user')
+    }
   }
 
-  onConnected(connectionId) {
-    console.log('connection id', connectionId)
+  onConnected(id) {
+    console.log('connection id', id)
+    this.connId = id;
   }
 }

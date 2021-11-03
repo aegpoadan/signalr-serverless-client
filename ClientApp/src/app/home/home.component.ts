885920df5d0215a4a7a9edcfcfd807fccc7e9219
Andrew Hayes <Andrew.Hayes@abcsupply.com>
2021-11-02 19:55:50 -0500

    SignalR with custom Auth POC

--- ClientApp/src/app/home/home.component.ts
+++ ClientApp/src/app/home/home.component.ts
@@ -1,7 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, Inject } from '@angular/core';
 import { HubConnection, HubConnectionBuilder } from '@microsoft/signalr';
-import { ReturnObj } from '../models/returnObj.model';
 
 @Component({
   selector: 'app-home',
@@ -10,34 +9,39 @@ import { ReturnObj } from '../models/returnObj.model';
 export class HomeComponent {
   private conn: HubConnection;
   private username: string;
-  connId: string = 'test';
 
   constructor(private http: HttpClient, @Inject('BASE_URL') private baseUrl: string) {
     this.username = Math.floor(Math.random() * 10000) + '@gmail.com';
+    fetch('http://localhost:7071/api/token', {
+      method: 'POST'
+    }).then(response => {
+      response.json().then(tokenResponse => {
+        const token = tokenResponse.token;
+        this.conn = new HubConnectionBuilder()
+          .withUrl("http://localhost:7071/api", {
+            headers: {
+              "x-ms-signalr-user-id": this.username,
+              'X-Token': token
+            }
+          })
+          .build();
+        console.log('connecting...')
 
-    this.conn = new HubConnectionBuilder()
-      .withUrl("http://localhost:7071/api", {
-        headers: {
-          "x-ms-signalr-user-id": this.username
-        }
-      })
-      .build();
-    console.log('connecting...')
+        this.conn.on("userAdded", this.onUserAdded)
+        this.conn.on("connected", this.onConnected)
 
-    this.conn.on("userAdded", this.onUserAdded.bind(this))
-    this.conn.on("connected", this.onConnected.bind(this))
-
-    this.conn.start()
-    .then((resp) => {
-      console.log('connection established')
-    })
-    .catch(err => {
-      console.log(err)
-    })
+        this.conn.start()
+          .then((resp) => {
+            console.log('connection established')
+          })
+          .catch(err => {
+            console.log(err);
+          });
+      });
+    });
   }
 
   invokeEvent() {
-    console.log(this.connId)
     this.http.get(this.baseUrl + 'SignalRConnection/' + this.username).subscribe(result => {
       console.log(result)
     }, error => console.error(error));
@@ -49,16 +53,11 @@ export class HomeComponent {
     }, error => console.error(error));
   }
 
-  onUserAdded(returnObj: ReturnObj) {
-    if (returnObj.TriggeringUser.toLowerCase() !== this.username.toLowerCase()) {
-      alert(`${returnObj.TriggeringUser} added to group`)
-    } else {
-      console.log('came from same user')
-    }
+  onUserAdded(userId) {
+    console.log('user added', userId)
   }
 
-  onConnected(id) {
-    console.log('connection id', id)
-    this.connId = id;
+  onConnected(connectionId) {
+    console.log('connection id', connectionId)
   }
 }
--- Controllers/SignalRConnectionController.cs
+++ Controllers/SignalRConnectionController.cs
@@ -5,6 +5,9 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using System.Net.Http;
+using Newtonsoft.Json.Linq;
+using Newtonsoft.Json;
 
 namespace serverless_signalr_poc_client.Controllers
 {
@@ -12,36 +15,52 @@ namespace serverless_signalr_poc_client.Controllers
     [ApiController]
     public class SignalRConnectionController : ControllerBase
     {
+        private static readonly HttpClient client = new HttpClient();
+
         [HttpGet("{username}")]
         public async Task<IActionResult> Get(string username)
         {
-            var hubConnectionBuilder = new HubConnectionBuilder()
-                .WithUrl("http://localhost:7071/api", options => {
-                    options.Headers.Add("x-ms-signalr-user-id", username);
-                });
+            var tokenResponse = await client.PostAsync("Http://localhost:7071/api/token", new StringContent(string.Empty));
+            if (tokenResponse.IsSuccessStatusCode)
+            {
+                var responseBody = await tokenResponse.Content.ReadAsStringAsync();
+                var token = JsonConvert.DeserializeObject<JObject>(responseBody)["token"].ToString();
+                var hubConnectionBuilder = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:7071/api", options => {
+                        options.Headers.Add("x-ms-signalr-user-id", username);
+                        options.Headers.Add("X-Token", token);
+                    });
 
-            var conn = hubConnectionBuilder.Build();
-            await conn.StartAsync();
+                var conn = hubConnectionBuilder.Build();
+                await conn.StartAsync();
 
-            await conn.InvokeAsync("joinUserToGroup", username, "group1");
+                await conn.InvokeAsync("joinUserToGroup", username, "group1");
 
-            return Ok();
+                return Ok();
+            } else return Unauthorized();
         }
 
         [HttpGet("InvokeError/{username}")]
         public async Task<IActionResult> InvokeError(string username)
         {
-            var hubConnectionBuilder = new HubConnectionBuilder()
-                .WithUrl("http://localhost:7071/api", options => {
-                    options.Headers.Add("x-ms-signalr-user-id", username);
-                });
+            var tokenResponse = await client.PostAsync("Http://localhost:7071/api/token", new StringContent(string.Empty));
+            if (tokenResponse.IsSuccessStatusCode)
+            {
+                var responseBody = await tokenResponse.Content.ReadAsStringAsync();
+                var token = JsonConvert.DeserializeObject<JObject>(responseBody)["token"].ToString();
+                var hubConnectionBuilder = new HubConnectionBuilder()
+                    .WithUrl("http://localhost:7071/api", options => {
+                        options.Headers.Add("x-ms-signalr-user-id", username);
+                        options.Headers.Add("X-Token", token);
+                    });
 
-            var conn = hubConnectionBuilder.Build();
-            await conn.StartAsync();
+                var conn = hubConnectionBuilder.Build();
+                await conn.StartAsync();
 
-            await conn.InvokeAsync("joinUserError", username, "group1");
+                await conn.InvokeAsync("joinUserError", username, "group1");
 
-            return Ok();
+                return Ok();
+            } else return Unauthorized();
         }
     }
 }
